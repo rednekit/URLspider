9247ccc043a944431e046d97576479123db72676
rednekit <npkotkov@yandex.ru>
2024-06-22 16:37:24 +0300

    add results to html file

--- URL spider.py
+++ URL spider.py
@@ -5,7 +5,9 @@ from time import gmtime, strftime
 import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
            'referer': 'https://google.com'}
-def replace_nik(a, durl):
+def create_HTML():
+    pass
+def replace_nik(a, durl):#замена относительных ссылок на абсолютные
     begin=0
     z=a.find('href="/', begin)
     while z != -1:
@@ -19,6 +21,13 @@ def replace_nik(a, durl):
     a.replace('///','//')
     return(a)
 def spider(URL):
+    global index
+    if URL.find('/', URL.find('//') + 2) != -1:
+        dURL = URL[URL.find('//') + 2:URL.find('/', URL.find('//') + 1)]  # вычленяем доменное имя из адреса сайта
+    elif URL.find('?', URL.find('//') + 1) != -1:
+        dURL = URL[URL.find('//') + 2:URL.find('?', URL.find('//') + 1)]  # вычленяем имя страницы из адреса сайта
+    else:
+        dURL = URL[URL.find('//') + 2:]  # вычленяем имя страницы из адреса сайта
     try:
         req = requests.get(URL, headers, timeout=3)
     except requests.exceptions.Timeout:
@@ -59,19 +68,11 @@ def spider(URL):
         else:#если гиперссылка абсолютная и в ней нет слеша или "?", то
             domain_text1 = text1[text1.find('//') + 1:]#находим адрес страницы
 
-
-        if URL.find('/', URL.find('//') + 2) != -1:
-            dURL=URL[URL.find('//') + 2:URL.find('/', URL.find('//') + 1)]# вычленяем доменное имя из адреса сайта
-        elif URL.find('?', URL.find('//')+1) != -1:
-            dURL = URL[URL.find('//') + 2:URL.find('?', URL.find('//') + 1)]# вычленяем имя страницы из адреса сайта
-        else:
-            dURL = URL[URL.find('//') + 2:]# вычленяем имя страницы из адреса сайта
-        count+=1
         if e.find(text1)==-1 and text1[:4]=='http' and domain_text1 != dURL:#проверяем ссылку на уникальность
             if text1 not in f:
                 e = e + text1 + "\n"
     if keyword in data:
-        file3 = open("URL"+URL[8:100].replace("/","_")+".html", "a")
+        file3 = open("URL"+URL[8:100].replace("/","_").replace("?","_")+".html", "a", encoding="utf-8")
         print("Bingo!")
         data=replace_nik(data, dURL)
         data.replace('///', '//')
@@ -79,11 +80,14 @@ def spider(URL):
         file3.close()
         file1 = open("found.txt", "a")
         file1.write(URL+"\n")
+        index+='<p><a href='+URL+'>'+URL+'</a>'+"</p>\n"
         file1.close()
     file = open("result.txt", "a")
     file.write(e)
     file.close()
     return(count)
+global index
+index="<!DOCTYPE html>"+"\n"+"<html lang='ru'>"+"\n"+"    <head>"+"\n"+"        <meta charset='utf-8'>"+"\n"+"        <title>Новое исследование</title>"+"\n"+"    </head>"+"\n"+"    <body>"+"\n"+"       <H1>Search results:</H1>"+"\n"
 print("Введите ключевую фразу")
 keyword=input()
 print("Введите адрес сайта с которого начнется поиск")
@@ -98,3 +102,7 @@ for i in range(COI):
     end=site_list.find('\n', begin)
     print(spider(site_list[begin:end]), site_list[begin:end])
     begin=end+1
+index+="    </body>"+"\n"+"</html>"
+file=open("index2.html","a")
+file.write(index)
+file.close()
